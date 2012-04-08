70f1bde9590b495247684501ff5f04004629bf04
Mona Ghassemi <monagz@gmail.com>
2012-04-08 16:02:17 -0400

    added skeleton comments to files

---
+++ PART1/MAIN.asm
@@ -0,0 +1,3 @@
+;; Your main program can be anything you like: for example display a few mode code numbers,
+;; asks the user to input a mode code number, switch to that mode and then draw a few pixels
+;; and lines in different pen colors.
