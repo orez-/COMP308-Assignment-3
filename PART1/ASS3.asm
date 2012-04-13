42ecd2b3f14dd1e92d53f8ab5fcb4faf9ced77db
orez- <bshaginaw120@gmail.com>
2012-04-13 16:35:16 -0400

    Started drawpixel, need to accept parameters.

--- PART1/ASS3.asm
+++ PART1/ASS3.asm
@@ -2,55 +2,26 @@
 .model huge
 .stack 100h
 .data
-    sprompt db "Enter a positive integer: ",0
-    
+	PENCOLOR db 0
+
 .data?
     buffer db 100 DUP(?)
+
 .code
 start:
-    jmp .end
-    
-    ; SKIP ME
-    mov ax, @data
-    mov ds, ax
-    
-    mov ax, OFFSET sprompt  ; prepare to write the string
-    push ax                 ; ax is the argument
-    call puts               ; to puts
-    add sp, 2               ; pop without putting it anywhere
-    
-    mov ax, OFFSET buffer
-    push ax
-    call GetInt ; get your integer number
-    add sp, 2
-    
-    xor ah, ah  ; set ah to 0 (quickly, apparently)
-    push ax     ; add ah (0) : al (our int) to the stack to be read by PrintInt later
-    mov cl, al  ; set cl as our counter
-    
-    .loop_as:
-        cmp cl, 0   ; if you have counted down to 0
-        je .end_as  ; end
-        push 'A'    ; print an A
-        call putch
-        add sp, 2
-        dec cl      ; countdown
-        jmp .loop_as
-    .end_as:
-    
-    push 13     ; print a carriage return
-    call putch
-    add sp, 2
-    push 10     ; print a newline
-    call putch
-    add sp, 2
-    
-    call PrintInt   ; print the number (which we loaded onto the stack a while ago)
-    add sp, 2
-    
-;.end:
-    ;mov ah, 4ch     ; exit program
-    ;int 21h
+	mov ax, @data
+	mov ds, ax
+	
+	mov ah, 0
+	mov al, 13
+	call setmode
+	mov ah, 10
+	call setpencolor
+	call drawpixel
+	
+.end:
+    mov ah, 4ch
+    int 21h
 
 getche:
     mov ah, 1       ; move 'read char from stdin' to ah
@@ -181,5 +152,69 @@ PrintInt:
     pop bp
     ret
 
-.end:
+
+
+;;this is where the api goes
+
+;; bool AL SETMODE(int AH)
+;; permits user to set to some mode; if not work, autoreset to standard text mode,
+;; return false
+
+;; change screen mode 
+	;; mov ah 0
+;; this is the mode it will change to
+setmode:	
+	;mov al, ah    ; wat?
+	mov al, 13h
+	mov ah, 0
+	int 10h
+	ret
+;; bool AL SETPENCOLOR(int COLOR)
+;; By providing the color number as a parameter the API will remember that you are
+;; currently using that colored pencil. The function returns false if it encounters
+;; a problem. You will need a variable in memory to store this information, call it
+;; PENCOLOR.
+setpencolor:
+	push bp
+	mov bp, sp
+
+	;call getInt 		;which stores stuff in al
+	mov al, 1100b
+	mov PENCOLOR, al
+	;; cmp blah
+	;jne blah
+	;mov al 0
+	;ret
+	
+	pop bp
+	ret
+;;bool AL DRAWPIXEL(int X, int Y)
+;; Using the pen color and mode you already selected set pixel at X, Y to color and
+;; returns false if there was a problem. Important: this function does not use the
+;; BIOS but accesses the memory directly.
+drawpixel:
+    mov bh, 0
+    mov al, PENCOLOR
+    mov cx, 10  ; x
+    mov dx, 20  ; y
+    mov ah, 0Ch  ; draw pixel
+    int 10h
+	ret
+;;bool AL DRAWLINE(int X, int Y, int X2, int Y2)
+;; Using the pen color and the mode you already selected this function draws a
+;; general line from the two coordinates in any direction. This is a general purpose
+;; line drawing function. It returns false if there was a problem. Note that this
+;; function uses DRAWPIXEL to actually write on the screen buffer.
+drawline:
+    push bp
+    mov bp, sp
+    
+    
+    
+    
+	pop di 			; cleanup etc
+	mov sp, bp
+	pop bp
+	ret
+	
 END start
--- PART1/GAPI.asm
+++ PART1/GAPI.asm
@@ -9,7 +9,19 @@ include IO.asm
 .code
 start:
 	mov ax, @data
-	modv ds, ax
+	mov ds, ax
+	
+	
+	jmp .end
+	call setmode
+	mov ah 2
+	call setpencolor
+	call drawpixel
+	
+.end:
+    mov ah, 4ch
+    int 21h
+	
 ;;this is where the api goes
 
 ;; bool AL SETMODE(int AH)
@@ -20,9 +32,9 @@ start:
 	;; mov ah 0
 ;; this is the mode it will change to
 setmode:	
-	mov al [ah]
+	mov al [ah]    ; wat?
 	mov ah 0
-	int 10
+	int 10h
 	ret
 ;; bool AL SETPENCOLOR(int COLOR)
 ;; By providing the color number as a parameter the API will remember that you are
@@ -46,7 +58,12 @@ setpencolor:
 ;; Using the pen color and mode you already selected set pixel at X, Y to color and
 ;; returns false if there was a problem. Important: this function does not use the
 ;; BIOS but accesses the memory directly.
-drawpixel:	
+drawpixel:
+    mov ah 0Ch  ; draw pixel
+    mov al PENCOLOR
+    mov cx 200  ; x
+    mov dx 200  ; y
+    int 10h
 	ret
 ;;bool AL DRAWLINE(int X, int Y, int X2, int Y2)
 ;; Using the pen color and the mode you already selected this function draws a
